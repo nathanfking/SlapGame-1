5501b221b2bf703f7f4de5d1cb6f5506ae17b317
Nathan King <nathan.f.king@gmail.com>
2016-09-23 15:27:40 -0600

    a

---
+++ game.js
@@ -0,0 +1,35 @@
+function player(health, )
+var health = 100;
+
+function onSlap() {
+    var slap = Math.floor(Math.random()*10)+1
+    health -= slap
+    update()
+    return health
+    }
+   
+    function onPunch() {
+        var punch = Math.floor(Math.random()*20)+1
+        health -= punch
+        update()
+        return health
+    }
+    function onKick() {
+        var kick = Math.floor(Math.random()*40)+1
+        health -= kick
+        update ()
+        return health
+    }
+   
+    function reset() {
+        health = 100
+        update()
+    }
+     function update() {
+    if (health <= 0) 
+        health = 0
+    
+    var healthElem = document.getElementById("health")
+    healthElem.innerHTML = health
+
+    }
