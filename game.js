7f80146b5f36b7d759facc089b496aa11ada229d
Nathan King <nathan.f.king@gmail.com>
2016-09-23 23:37:27 -0600

    a

--- game.js
+++ game.js
@@ -1,35 +1,41 @@
-function player(health, )
 var health = 100;
-
+// this is player object
+function player (johnDoe,janeDoe,tomDoe,tinaDoe){
+    this.johnDoe = johnDoe;
+    this.janeDoe = janeDoe;
+    this.tomDoe = tomDoe;
+    this.tinaDoe = tinaDoe;
+}
+// This is attack functions
 function onSlap() {
-    var slap = Math.floor(Math.random()*10)+1
-    health -= slap
-    update()
-    return health
+    var slap = Math.floor(Math.random()*10)+1;
+    health -= slap;
+    update();
+    return health;
     }
    
     function onPunch() {
-        var punch = Math.floor(Math.random()*20)+1
-        health -= punch
-        update()
-        return health
+        var punch = Math.floor(Math.random()*20)+1;
+        health -= punch;
+        update();
+        return health;
     }
     function onKick() {
-        var kick = Math.floor(Math.random()*40)+1
-        health -= kick
-        update ()
-        return health
+        var kick = Math.floor(Math.random()*40)+1;
+        health -= kick;
+        update ();
+        return health;
     }
    
     function reset() {
-        health = 100
-        update()
+        health = 100;
+        update();
     }
      function update() {
-    if (health <= 0) 
-        health = 0
+    if (health <= 0); 
+        health = 0;
     
-    var healthElem = document.getElementById("health")
-    healthElem.innerHTML = health
+    var healthElem = document.getElementById("health");
+    healthElem.innerHTML = health;
 
     }
